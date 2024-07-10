f91530388f4442a05d32ea003dba21e6e17f36a4
Vindya <vindya092@gmail.com>
2024-07-10 13:34:39 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,8 +14,9 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
+  setAttribute: (name: string, value: string) => void,
 }
 
 /**
@@ -32,7 +33,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -46,26 +47,35 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
+      elem.load(this.table);
 
       // Add more Perspective configurations here.
-      elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('aggregates', JSON.stringify({
+        stock: 'distinct count',
+        top_ask_price: 'avg',
+        top_bid_price: 'avg',
+        timestamp: 'distinct count',
+      }));
     }
   }
 
   componentDidUpdate() {
     // Everytime the data props is updated, insert the data into Perspective table
     if (this.table) {
-      // As part of the task, you need to fix the way we update the data props to
-      // avoid inserting duplicated entries into Perspective table again.
-      this.table.update(this.props.data.map((el: any) => {
-        // Format the data from ServerRespond to the schema
+      // Format the data from ServerRespond to the schema
+      const formattedData = this.props.data.map((el: ServerRespond) => {
         return {
           stock: el.stock,
           top_ask_price: el.top_ask && el.top_ask.price || 0,
           top_bid_price: el.top_bid && el.top_bid.price || 0,
-          timestamp: el.timestamp,
+          timestamp: new Date(el.timestamp),
         };
-      }));
+      });
+      this.table.update(formattedData as unknown as {[key: string]: any[]});
     }
   }
 }
